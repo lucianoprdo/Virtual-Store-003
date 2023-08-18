ba53fc1006e71ab6785849c382057336f8c6b66e
Gabriel Mouro <gabrielmouro38@gmail.com>
2023-08-18 12:29:25 -0300

    recebendo informaçoes do github

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import * as api from '../services/api';
 
 function Home() {
+  const [pesquisar, setPesquisar] = useState('');
+
+  async function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const newSearch = event.target.value;
+    // console.log(newSearch);
+    setPesquisar(newSearch);
+    // console.log(await getProductsFromCategoryAndQuery('','Games'));
+    // console.log(await getCategories());
+    console.log(await api.getCategories());
+    console.log(await api.getProductsFromCategoryAndQuery('', pesquisar));
+  }
+
+  /*const handleSearch = async () => {
+    const resultado = await getProductsFromCategoryAndQuery(pesquisar);
+    setPesquisar('');
+    setLoaging(false);
+  };*/
+
   return (
     <div>
-      <input type="text" placeholder="Digite o que você busca" />
+      <input
+        type="text"
+        placeholder="Digite o que você busca"
+        onChange={ (event) => handleChange(event) }
+      />
       <h2 data-testid="home-initial-message">
         Digite algum termo de pesquisa ou escolha uma categoria.
       </h2>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 const URL_ENDPOINT = 'https://api.mercadolibre.com/sites/MLB/categories';
 // const URL_TERMO = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
 // const URL_CATEGORY_ID = 'https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID';
-const URL_CATEGORY_QUERY = 'https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID&q=$QUERY';
+// const URL_CATEGORY_QUERY = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${QUERY}`;
 
 const getCategories = async () => {
   const getEndCategories = await fetch(URL_ENDPOINT);
@@ -10,11 +10,11 @@ const getCategories = async () => {
 };
 getCategories();
 
-const getProductsFromCategoryAndQuery = async () => {
-  const getProductsCategory = await fetch(URL_CATEGORY_QUERY);
+const getProductsFromCategoryAndQuery = async (categoryId: string, query: string) => {
+  const getProductsCategory = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   const response = await getProductsCategory.json();
   return response;
-  console.log(response);
+  // console.log(response);
 };
 // getProductsFromCategoryAndQuery();
 
